ca6910caae5431cc29b53a8846c00c82dfd3fb68
sovni <christophe.burgalat@gmail.com>
2021-12-01 22:15:07 +0100

    Updates

---
+++ precache-manifest.9221b8d1d53e678859773a4a7756d414.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "390d7fa827c318038ceb09e16868457d",
+    "url": "/simplestorage-ghpages/index.html"
+  },
+  {
+    "revision": "cef942b4dc4027f518cb",
+    "url": "/simplestorage-ghpages/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "dbf5be947faf7a745a91",
+    "url": "/simplestorage-ghpages/static/js/2.d190b924.chunk.js"
+  },
+  {
+    "revision": "cef942b4dc4027f518cb",
+    "url": "/simplestorage-ghpages/static/js/main.66165982.chunk.js"
+  },
+  {
+    "revision": "8a0350bb5bc91c68d183",
+    "url": "/simplestorage-ghpages/static/js/runtime-main.2057997f.js"
+  }
+]);
